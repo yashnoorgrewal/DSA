b886afe50efa5cac5a6c2dc2c27f718f8059c778
yashnoorgrewal <155097063+yashnoorgrewal@users.noreply.github.com>
2025-06-10 01:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (87.26%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,38 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1 = -1;
-        int ind2  = -1;
-        for(int i = nums.length - 2; i >= 0; i--){
-            if(nums[i] < nums[i+1]){
-                ind1 = i;
+        int n = nums.length;
+        int gola_index = -1;
+
+        //Find the first decreasing element from the end
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                gola_index = i - 1;
                 break;
             }
-        } 
-        if(ind1 == -1){
-            reverse(nums, 0);
-        }else{
-            for(int i = nums.length - 1; i >= 0; i--){
-                if(nums[i] > nums[ind1]){
-                    ind2 = i;
+        }
+
+        //If found, find the just larger element to the right and swap
+        if (gola_index != -1) {
+            int swap_index = gola_index;
+            for (int j = n - 1; j > gola_index; j--) {
+                if (nums[j] > nums[gola_index]) {
+                    swap_index = j;
                     break;
                 }
             }
-            swap(nums, ind1, ind2);
-            reverse(nums, ind1+1);
+            int temp = nums[gola_index];
+            nums[gola_index] = nums[swap_index];
+            nums[swap_index] = temp;
         }
-    }
-    void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    void reverse(int[] nums, int start){
-        int i = start;
-        int j = nums.length-1;
-        while(i < j){
-            swap(nums, i, j);
-            i++;
-            j--;
+
+        //reverse what is all there on the right side of gola_index (i-1)
+        int start = gola_index + 1;
+        int end = n - 1;
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
