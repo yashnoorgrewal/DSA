91b130c0fbb0206f4df947d302af64abca791860
yashnoorgrewal <155097063+yashnoorgrewal@users.noreply.github.com>
2025-08-09 23:05:04 +0530

    Time: 110 ms (94.88%) | Memory: 42 MB (29.75%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        //make a character array of word
+        char[] word_arr = word.toCharArray();
+        //iterate
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==word_arr[0] && exists(board,i,j,word_arr,0)){
+                    return true;
+                }
+            }
+        }return false;
+    }
+    //exists function
+    public static boolean exists(char[][] board, int i, int j, char[] word, int index){
+        //base conditions
+        if(i<0||i>=board.length || j<0||j>=board[0].length || board[i][j]=='*' || board[i][j]!=word[index]) return false;
+
+        if(index == word.length-1){ //if this satisfies, we found the word in the board
+            return true;
+        }
+
+        char ch = board[i][j];
+        board[i][j] = '*'; //mark the cells already visited
+        if(exists(board,i+1,j,word,index+1)||
+        exists(board,i-1,j,word,index+1)||
+        exists(board,i,j+1,word,index+1)||
+        exists(board,i,j-1,word,index+1)){
+            return true;
+        }
+        board[i][j] = ch; //Backtrack
+        return false;
+    }
+}
